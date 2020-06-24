47cabaee64c1d7d721bc0c982ba529c374b92a26
Leonardo Burati <leonardoburati@hotmail.com>
2020-06-24 13:15:20 -0300

    Challenge complete!

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const ValidateTotal = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > ValidateTotal.total) {
+      throw Error('Outcome is bigger than your Total');
+    }
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type must be income or outcome');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
